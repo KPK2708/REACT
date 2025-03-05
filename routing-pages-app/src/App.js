c92eebdbdb61409f57e2cf0a574665c7fa6dc535
katta praveen kumar <praveenkumarkatta07@gmail.com>
2025-03-05 15:33:24 +0530

    intial commit

--- routing-pages-app/src/App.js
+++ routing-pages-app/src/App.js
@@ -1,35 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import AboutPage from "./components/AboutPage";
+import "./styles.css";
 
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import './styles.css';  // Import the external CSS
-
-import HomePage from './components/HomePage';
-import AboutPage from './components/AboutPage';
-
-
-function App() {
+const App = () => {
   return (
     <Router>
-      <div className="navbar">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </div>
-      
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-      </Switch>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
